c18d4a44ba984024e8030824ad3a0e648e0e9c59
Najoro <najofanantenana@gmail.com>
2022-03-27 13:52:44 +0300

    end html css

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const beurgeur = document.getElementById("beurgeur");
+const menu = document.querySelector('.menu-bare');
+const header = document.querySelector('header');
 
-
-const beurgeur = document.getElementById("beurgeur-menu")
-const menu = document.querySelector('.menu-bare')
+console.log(header);
 
 beurgeur.addEventListener("click", () => {
      beurgeur.classList.toggle('active')
      menu.classList.toggle('visible')
+     header.classList.toggle('scale')
   
 })
 
