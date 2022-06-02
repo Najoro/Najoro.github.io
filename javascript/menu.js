502d37de7acb9e31b0c3c1759780840eb7844eb7
Najoro <najofanantenana@gmail.com>
2022-06-02 18:56:54 +0300

    organisation header

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,50 +1,33 @@
-
 // //DECLARATIONS
 const beurgeur = document.getElementById("beurgeur");
-const menu = document.querySelector('.menu');
-const header = document.querySelector('header');
+const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
 
-(function(){
-     let showMenu = function(){
-          beurgeur.classList.toggle('activeJS');
-          menu.classList.toggle('showMenu');
-     }
-     beurgeur.addEventListener("click", showMenu,true);
+(function () {
+  let showMenu = function () {
+    beurgeur.classList.toggle("activeJS");
+    menu.classList.toggle("showMenu");
+  };
+  beurgeur.addEventListener("click", showMenu, true);
 })();
 
-// let rectBoun = header.getBoundingClientRect()
-// var largeur = rectBoun.width;
-// let hauteur = rectBoun.height;
-// let newElement = document.createElement('div');
+(() => {
+  const bull = document.querySelector(".bull");
 
-// newElement.style.width = largeur + 'px';
-// newElement.style.height = hauteur + "px";
-
-// //FONCTIONS
-
-// let scrollY = function(){
-//      var supportPageOffset = window.pageXOffset !== undefined;
-//      var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-//      return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-// }
-// let haut =rectBoun.top + scrollY();
-
-// let headerFixed = function (){
-//      let hasScroll = header.classList.contains('fixed')
-//      if(scrollY() > haut && !hasScroll){
-//           header.classList.add('fixed');
-//           document.body.insertBefore(newElement, header.nextElementSibling );
-          
-//      }
-//      else if(scrollY() <= haut && hasScroll){
-//           header.classList.remove('fixed');
-//           header.parentNode.removeChild(newElement);
-//      }
-// }
-// // APPEL
-
-// //  voir le menu si on click sur l'icone beurgeur responsive
-
-
-// // le menu est fixer sur le top de la page si on scrolle 
-// window.addEventListener('scroll', headerFixed) 
+  let scrollY = function () {
+    var supportPageOffset = window.pageXOffset !== undefined;
+    var isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+    return supportPageOffset
+      ? window.pageYOffset
+      : isCSS1Compat
+      ? document.documentElement.scrollTop
+      : document.body.scrollTop;
+  };
+  window.addEventListener("scroll", (e) => {
+    if (scrollY() > 70) {
+      bull.style.visibility = "visible";
+    } else {
+      bull.style.visibility = "hidden";
+    }
+  });
+})();
