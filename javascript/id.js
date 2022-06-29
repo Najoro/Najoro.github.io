5e2cd5b79665fa920d8583fb3f29f13a3651cfb2
Najoro <najofanantenana@gmail.com>
2022-06-30 00:28:47 +0300

    desieme jour identiter

--- javascript/id.js
+++ javascript/id.js
@@ -1,15 +1,37 @@
-const languages = document.querySelector(".languages");
-const diplomes = document.querySelector(".diplomes");
-const autre = document.querySelector(".autre");
-const interets = document.querySelector(".interets");
-const indetiter = [];
+const languagesBase = document.querySelector(".languages-base-contenaire");
+const languagesFramwork = document.querySelector(
+  ".languages-framwork-contenaire"
+);
+const autre = document.querySelector(".autres-contenaire");
+const diplomes = document.querySelector(".diplomes-contenaire");
+const interets = document.querySelector(".interets-contenaire");
+let data = [];
 
-const fetchUp = async function () {
+async function fetchUp() {
   await fetch("bd.json")
-    .then((res) => res.json())
-    .then((res) => res.identiter)
-    .then((res)=> indetiter.push(res));
+    .then((res1) => res1.json())
+    .then((res2)=> data = res2.identiter)
 
-  console.log(indetiter[0].language);
+  console.log(data);
 };
-fetchUp();
+
+const fetchDisplay = async function (tag, ref) {
+  const languages = ref;
+  languages.map((language) => {
+    tag.innerHTML += `
+    <div class="eatchLanguage">
+      <h4>${language.nom}</h4>
+      <div class="progresseBarre">
+        <div class="eatch-Bare" style = "width:${language.pourcentage}%;"></div>
+      </div>
+    </div>
+    `
+  });
+};
+async function fetchCall(){
+  await fetchUp();
+  fetchDisplay(languagesBase, data.languages);
+  fetchDisplay(languagesFramwork, data.framworks);
+  fetchDisplay(autre, data.autre);
+}
+fetchCall();
