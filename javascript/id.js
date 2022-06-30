3e2295aec143e06b2cf9a450431fe2e1b4d6e13c
Najoro <najofanantenana@gmail.com>
2022-06-30 23:54:47 +0300

    identiter2

--- javascript/id.js
+++ javascript/id.js
@@ -2,7 +2,8 @@ const languagesBase = document.querySelector(".languages-base-contenaire");
 const languagesFramwork = document.querySelector(
   ".languages-framwork-contenaire"
 );
-const autre = document.querySelector(".autres-contenaire");
+const autre1 = document.querySelector(".autres-contenaire1");
+const autre2 = document.querySelector(".autres-contenaire2");
 const diplomes = document.querySelector(".diplomes-contenaire");
 const interets = document.querySelector(".interets-contenaire");
 let data = [];
@@ -10,12 +11,19 @@ let data = [];
 async function fetchUp() {
   await fetch("bd.json")
     .then((res1) => res1.json())
-    .then((res2)=> data = res2.identiter)
+    .then((res2) => (data = res2.identiter));
 
   console.log(data);
+}
+const DisplayInteret = async function (tag, ref) {
+  const Interets = ref;
+  Interets.map((Interet) => {
+    tag.innerHTML += `
+    <li>${Interet.nom}</li>
+    `;
+  });
 };
-
-const fetchDisplay = async function (tag, ref) {
+const DisplayLanguages = async function (tag, ref) {
   const languages = ref;
   languages.map((language) => {
     tag.innerHTML += `
@@ -25,13 +33,29 @@ const fetchDisplay = async function (tag, ref) {
         <div class="eatch-Bare" style = "width:${language.pourcentage}%;"></div>
       </div>
     </div>
-    `
+    `;
   });
 };
-async function fetchCall(){
+const DisplayDiplome = async function (tag, ref) {
+  const languages = ref;
+  languages.map((language) => {
+    tag.innerHTML += `
+
+      <ul class="DiplomeEatch">
+        <li>${language.nom}</li>
+        <li>${language.obtention}</li>
+        <li>${language.ecole}</li>
+      </ul>
+    `;
+  });
+};
+async function fetchCall() {
   await fetchUp();
-  fetchDisplay(languagesBase, data.languages);
-  fetchDisplay(languagesFramwork, data.framworks);
-  fetchDisplay(autre, data.autre);
+  DisplayLanguages(languagesBase, data.languages);
+  DisplayLanguages(languagesFramwork, data.framworks);
+  DisplayLanguages(autre1, data.autre[0]);
+  DisplayLanguages(autre2, data.autre[1]);
+  DisplayInteret(interets.firstElementChild, data.interet);
+  DisplayDiplome(diplomes, data.Diplome);
 }
 fetchCall();
