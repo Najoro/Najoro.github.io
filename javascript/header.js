1cd79c1666e36c8e82a78016500ccc8e6108f556
najoro <najofanantenana@gmail.com>
2022-07-19 22:41:21 +0300

    finalisation du project

--- javascript/header.js
+++ javascript/header.js
@@ -1,52 +1,79 @@
 const childsContainer = document.querySelector(".contenaire").childNodes;
-const Menu = document.querySelector(".menu");
-const as = document.querySelectorAll('.menu a');
-const date = document.querySelector('.date');
+const beurgeur = document.getElementById("beurgeur");
+const header = document.querySelector("header");
+const menu = document.querySelector(".menu");
+const date = document.querySelector(".date");
+const as = document.querySelectorAll(".menu a");
 
-// function DateNow(){
-//   let Datenow = new Date()
-//   console.log(Datenow.toISOString());
-//   date.innerHTML = `
-//   ${Datenow.toISOString().split('T')[1].split(".")[0]}
-//   `
-// }
-// DateNow();
+//funtions ....
 
-console.log(childsContainer.nodeType);
-
-as.forEach((a)=> {
-  a.addEventListener('click', (e) => {
-       const active = a.parentElement.querySelector('.active')
+function Change_menu_active() {
+  as.forEach((a) => {
+    a.addEventListener("click", (e) => {
+      const active = a.parentElement.querySelector(".active");
   
-       //on retire la class active de n'import quelle element
-       active.classList.remove('active');
-
-       // ajouter la class active de l'element a click
-       if(!a.classList.contains('click')){
-            a.classList.add('active');
-       }
-  })
-})
-
-Menu.childNodes.forEach((child) => {
-
-  let handleActive = (childs, C, cur) => {
-    childs.forEach((element) => {
-      if (element.nodeType !== 3) {
-       if (element.id == cur) {
-            element.classList.remove(C) 
-       }
-        else {
-          if (!element.classList.contains(C)) {
-            element.classList.add(C);
-          }
-        }
+      //on retire la class active de n'import quelle element
+      active.classList.remove("active");
+  
+      // ajouter la class active de l'element a click
+      if (!a.classList.contains("click")) {
+        a.classList.add("active");
       }
     });
-  };
-  
-  child.addEventListener("click", (e) => {
-    let curentElement = child.textContent.toLowerCase();
-    handleActive(childsContainer, "desactive", curentElement);
   });
-});
+}
+
+function change_Page() {
+  menu.childNodes.forEach((child) => {
+    let handleActive = (childs, C, cur) => {
+      childs.forEach((element) => {
+        if (element.nodeType !== 3) {
+          if (element.id == cur) {
+            element.classList.remove(C);
+          } else {
+            if (!element.classList.contains(C)) {
+              element.classList.add(C);
+            }
+          }
+        }
+      });
+    };
+    child.addEventListener("click", (e) => {
+      let curentElement = child.textContent.toLowerCase();
+      handleActive(childsContainer, "desactive", curentElement);
+    });
+  });
+}
+
+function showMenu() {
+    beurgeur.classList.toggle("activeJS");
+    menu.classList.toggle("showMenu");
+};
+
+function Bull_if_scroll(){
+  const bull = document.querySelector(".bull");
+
+  let scrollY = function () {
+    var supportPageOffset = window.pageXOffset !== undefined;
+    var isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+    return supportPageOffset
+      ? window.pageYOffset
+      : isCSS1Compat
+      ? document.documentElement.scrollTop
+      : document.body.scrollTop;
+  };
+  window.addEventListener("scroll", (e) => {
+    if (scrollY() > 70) {
+      bull.style.visibility = "visible";
+    } else {
+      bull.style.visibility = "hidden";
+    }
+  });
+}
+
+//event....
+
+Change_menu_active();
+change_Page();
+Bull_if_scroll();
+beurgeur.addEventListener("click", showMenu, true);
