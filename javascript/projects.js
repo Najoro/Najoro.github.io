87e88ea0eea6611c99de89d0b287ef10277cb72d
najoro <najofanantenana@gmail.com>
2022-08-10 15:55:53 +0300

    changement de couleur

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,13 +1,21 @@
 const projects = document.getElementById("projects");
-let bds = [];
+let allProjects = [];
 
-let newProject = function (projectName, image,source,project) {
+let newProject = function (projectName,image,source,project,language) {
+
+  let objectJSON = JSON.stringify(language)
+  console.log(objectJSON);
+  //create new element
   let span = document.createElement("span");
   span.classList.add("composante");
   projects.appendChild(span);
+
+  //add class for the new element
   span.style.background = `${image} no-repeat center/cover`;
+
+  //add element for the new element
   span.innerHTML = `
-    <div class="language-contenaire"></div>
+    <div class="language-contenaire">${objectJSON}</div>
     <p> ${projectName}</p>
     <div class="btm-contenaire">
       <a href="${source}" id="external" class="btm-contenue"><i class="fa-solid fa-code"></i></a>
@@ -25,10 +33,10 @@ let newProject = function (projectName, image,source,project) {
 async function reclame_bd(){
   await fetch("bd.json")
   .then((rep1) => rep1.json())
-  .then((rep2) => bds = rep2.project);
+  .then((rep2) => allProjects = rep2.project);
 
-  bds.map((bd)=> {
-    newProject(bd.name, bd.image,bd.source, bd.project);
+  allProjects.map((Projects)=> {
+    newProject(Projects.name, Projects.image,Projects.source, Projects.project,Projects.language);
   })
 }
 reclame_bd();
